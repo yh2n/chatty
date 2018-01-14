b39f3aac383c3cf4f11bfaad1087fb6077a39735
yh2n <yohann.potico@gmail.com>
2018-01-14 00:57:13 -0500

    add CI

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { app, server } = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('homepage', () => {
+    it("should render html and return 200 status code", () => {
+        return chai.request(app)
+            .get('/')
+            .then((res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+            })
+    }) 
+})
