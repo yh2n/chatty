f7207afe1d0f7580bda4536ca785af0d06471628
yh2n <yohann.potico@gmail.com>
2018-01-13 23:51:20 -0500

    add CI

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
-const app = express();
+const socketIO = require('socket.io');
 const {PORT}  = require('./config');
+
+const app = express();
+const server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket => {
+    console.log("new connection");
+
+    socket.on('disconnect', () => {
+        console.log('user was disconnected from server')
+    }) 
+}))
+
+server.listen(PORT, () => {
     console.log(`server is up on port ${PORT}`, )
 });
