a77a2a852bbc80a4876bf3676caa8e56ed0f030c
yohann potico <yh2n@MacBook-Air-2.local>
2018-01-19 16:17:52 -0500

    communication between users, callback when messages emitted, form, display on screen

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const socketIO = require('socket.io');
 const { PORT }  = require('./config');
 const moment = require('moment');
 
+const { generateMessage } = require('../server/utils/message');
 const app = express();
 const server = http.createServer(app);
 let io = socketIO(server);
@@ -15,18 +16,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket => {
     console.log("new connection");
 
-    socket.emit('newMessage', {
-        from: "bro@msg.com",
-        text: "whaddup dog?",
-        sent: moment().format("dddd, MMMM Do, YYYY"),
-        at: moment().format("h:mm a")
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chatty'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('new message', newMessage);
-        io.emit(newMessage, {
-            
-        })
+    //1st argument to callback argument listis data being sent
+    //2nd one is callback fcunction that acknowledges request
+    socket.on('createMessage', (message, callback) => {
+        console.log('new message', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        //argument: data being sent to client
+        callback(`(from: server)`);
     })
 
     socket.on('disconnect', () => {
