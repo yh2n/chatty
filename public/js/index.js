841ffea83d6edb42ad8a3b5acf2ec350cc6738d6
yh2n <yohann.potico@gmail.com>
2018-01-20 16:19:18 -0500

    update new branch

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
     console.log("user connected to server");
-
-    socket.emit('createMessage', {
-        from: "dude@msg.com",
-        text: "All good man..."
-    })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -16,18 +16,27 @@ io.on('connection', (socket => {
     console.log("new connection");
 
     socket.emit('newMessage', {
-        from: "bro@msg.com",
-        text: "whaddup dog?",
+        from:"Admin",
+        text: "Welcome!",
+        sent: moment().format("dddd, MMMM Do, YYYY"),
+        at: moment().format("h:mm a")
+    })
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user joined",
         sent: moment().format("dddd, MMMM Do, YYYY"),
         at: moment().format("h:mm a")
     });
 
     socket.on('createMessage', (newMessage) => {
-        console.log('new message', newMessage);
-        io.emit(newMessage, {
-            
-        })
-    })
+         console.log('new message', newMessage);
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            sent: moment().format("dddd, MMMM Do, YYYY"),
+            at: moment().format("h:mm a")
+        });
+    });
 
     socket.on('disconnect', () => {
         console.log('user was disconnected from server')
