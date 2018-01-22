c8e6364f0c57e02110acc3c912e68a017cbc6662
yohann potico <yh2n@MacBook-Air-2.local>
2018-01-21 23:37:54 -0500

    removes space between latitude and longitude in url

--- test/message.test.js
+++ test/message.test.js
@@ -20,7 +20,7 @@ describe("generateLocationMessage", () => {
         let from = "Don";
         let latitude = 12;
         let longitude = 17;
-        let url = "https://google.com/maps?q=12, 17";
+        let url = "https://google.com/maps?q=12,17";
         let message = generateLocationMessage(from,latitude,longitude);
         console.log(message);
         expect(message).to.include({from, url});
