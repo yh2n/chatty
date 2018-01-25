98dc7a2cc56d9fd8c57ac191c461f9d40a06fd12
yohann potico <yh2n@MacBook-Air-2.local>
2018-01-25 12:14:55 -0500

    add mustache.js for templating

--- public/js/index.js
+++ public/js/index.js
@@ -11,39 +11,52 @@ socket.on('disconnect', function () {
 });
 
 //text <li>
-socket.on('newMessage', message => {
-    console.log("new message", message);
-    let li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+socket.on('newMessage', function(message) {
+    let template = $("#message-template").html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        at: moment().format("h:mm a")
+    });
 
-    $('#messages').append(li);
+    $("#messages").append(html);
 });
 
 //location <li>
 socket.on("newLocationMessage", message => {
-    let li = $('<li></li>');
-    let a = $('<a target="_blank">Current location</a>');   
-    li.text(`${message.from}: `);
-    a.attr("href", message.url);
-    li.append(a);
-    $("#messages").append(li);
+    // let li = $('<li></li>');
+    // let a = $('<a target="_blank">Current location</a>');   
+    // li.text(`${message.from}: `);
+    // a.attr("href", message.url);
+    // li.append(a);
+    // $("#messages").append(li);
+
+    let template = $("#location-message-template").html();
+    let html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        at: moment().format("h:mm a")
+    });
+    $("#messages").append(html);
 })
 
 // event emitter
 //3rd argument: callback function that's triggered when acknowledgement arrives
+let textField = $('[name=message]')
 $("#message-form").on("submit", (e) => {
     e.preventDefault();
     socket.emit('createMessage', {
         from: "user",
-        text: $('[name=message]').val()
+        text: textField.val()
     }, () => {
-
+        textField.val('')
     })
 })
 
 let locationBtn = $("#share_location");
 locationBtn.on("click", () => {
     //navigator.geolocation object doesn't exist
+    locationBtn.attr("disabled", "disabled").text("Sending location...");
     if(!navigator.geolocation) {
         return alert("Geolocation not supported by your browser.");
     }
@@ -52,12 +65,14 @@ locationBtn.on("click", () => {
         //2nd callback function for error handling
         navigator.geolocation.getCurrentPosition((position) => {
             console.table(position);
+            locationBtn.removeAttr("disabled").text("Share location");
             socket.emit("createLocationMessage", {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             }); 
         }, () => {
-            alert("Unable to get location")
+            locationBtn.removeAttr("disabled").text("Share location");
+            alert("Unable to get location");
         });
     }
 });
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket => {
         console.log('new message', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         //argument: data being sent to client
-        callback(`(from: server)`);
+        callback();
     });
 
     socket.on("createLocationMessage", (coords) => {
