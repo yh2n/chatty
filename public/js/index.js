f9d2f57a473b34928238e6449fc2defff3552f95
yh2n <yohann.potico@gmail.com>
2018-01-28 13:43:51 -0500

    merge with feature branch

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,6 @@ socket.on('disconnect', function () {
 });
 
 //text <li>
-<<<<<<< HEAD
 socket.on('newMessage', function(message) {
     let template = $("#message-template").html();
     let html = Mustache.render(template, {
@@ -18,19 +17,12 @@ socket.on('newMessage', function(message) {
         from: message.from,
         at: moment().format("h:mm a")
     });
-=======
-socket.on('newMessage', message => {
-    console.log("new message", message);
-    let li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
->>>>>>> 9adb6177bc05d733105b9656482e75bce0ea6eb3
 
     $("#messages").append(html);
 });
 
 //location <li>
 socket.on("newLocationMessage", message => {
-<<<<<<< HEAD
     let template = $("#location-message-template").html();
     let html = Mustache.render(template, {
         url: message.url,
@@ -38,22 +30,11 @@ socket.on("newLocationMessage", message => {
         at: moment().format("h:mm a")
     });
     $("#messages").append(html);
-=======
-    let li = $('<li></li>');
-    let a = $('<a target="_blank">Current location</a>');   
-    li.text(`${message.from}: `);
-    a.attr("href", message.url);
-    li.append(a);
-    $("#messages").append(li);
->>>>>>> 9adb6177bc05d733105b9656482e75bce0ea6eb3
 })
 
 // event emitter
 //3rd argument: callback function that's triggered when acknowledgement arrives
-<<<<<<< HEAD
 let textField = $('[name=message]')
-=======
->>>>>>> 9adb6177bc05d733105b9656482e75bce0ea6eb3
 $("#message-form").on("submit", (e) => {
     e.preventDefault();
     socket.emit('createMessage', {
@@ -67,10 +48,7 @@ $("#message-form").on("submit", (e) => {
 let locationBtn = $("#share_location");
 locationBtn.on("click", () => {
     //navigator.geolocation object doesn't exist
-<<<<<<< HEAD
     locationBtn.attr("disabled", "disabled").text("Sending location...");
-=======
->>>>>>> 9adb6177bc05d733105b9656482e75bce0ea6eb3
     if(!navigator.geolocation) {
         return alert("Geolocation not supported by your browser.");
     }
@@ -79,21 +57,14 @@ locationBtn.on("click", () => {
         //2nd callback function for error handling
         navigator.geolocation.getCurrentPosition((position) => {
             console.table(position);
-<<<<<<< HEAD
             locationBtn.removeAttr("disabled").text("Share location");
-=======
->>>>>>> 9adb6177bc05d733105b9656482e75bce0ea6eb3
             socket.emit("createLocationMessage", {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             }); 
         }, () => {
-<<<<<<< HEAD
             locationBtn.removeAttr("disabled").text("Share location");
             alert("Unable to get location");
-=======
-            alert("Unable to get location")
->>>>>>> 9adb6177bc05d733105b9656482e75bce0ea6eb3
         });
     }
 });
